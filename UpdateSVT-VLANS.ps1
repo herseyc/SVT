a1594add6938f34222819697b2cfd12974347134
Hersey <herseyc@gmail.com>
2016-09-15 09:36:31 -0400

    Create UpdateSVT-VLANS.ps1

---
+++ UpdateSVT-VLANS.ps1
@@ -0,0 +1,39 @@
+#######################################
+# PowerCLI to update VLANs on SimpliVity Storage and Federation PortGroups
+# For migration for SimpliVity Direct Connect to 10 GbE Switched
+#
+# Must be connected to vCenter using Connect-VIServer
+#
+# Script: UpdateSVT-VLANS.ps1
+#
+# History:
+# 09/15/2015 - Hersey http://www.vhersey.com/ - Created
+#######################################
+###############VARIABLES###############
+# vCenter Inventory Name of SimpliVity Hosts
+$SVTHosts = "svthost1.vhersey.com", "svthost2.vhersey.com"
+
+# SimpliVity Storage vmKernel Name
+$SVTStorageVMK = "SVT_StorageVMK"
+
+# SimpliVity Storage PortGroup Name
+$SVTStoragePG = "SVT_StoragePG"
+
+# SimpliVity Federation PortGroup Name
+$SVTFedPG = "SVT_FederationPG"
+
+# SimpliVity Storage VLAN
+$SVTStorageVLAN = "60"
+
+# SimpliVity Federation VLAN
+$SVTFedVLAN = "61"
+
+foreach ($SVTHost in $SVTHosts) {
+    $vmhost = Get-VMHost -Name $SVTHost 
+    Write-Host "Updating SimpliVity Virtual Networks for Host: $SVTHost"
+    $vmhost | Get-VirtualPortGroup -Name $SVTStoragePG | Set-VirtualPortGroup -VlanId $SVTStorageVLAN
+    $vmhost | Get-VirtualPortGroup -Name $SVTStorageVMK | Set-VirtualPortGroup -VlanId $SVTStorageVLAN
+    $vmhost | Get-VirtualPortGroup -Name $SVTFedPG | Set-VirtualPortGroup -VlanId $SVTFedVLAN 
+}
+
+Write-Host "Done"
