2e05e09205b18987aa8b8ca04e04773b5663e2ea
Hersey <herseyc@gmail.com>
2017-01-30 09:21:44 -0500

    Update SVT-Backup-Script.ps1

--- SVT-Backup-Script.ps1
+++ SVT-Backup-Script.ps1
@@ -1,3 +1,14 @@
+#######################################################################
+#
+# SVT-Backup-Script.ps1
+#
+# Loops through a list of VM Inventory names and executes SVT-BackupVM.ps1
+# to backup VMs in the list.
+# SVT-BackupVM.ps1 can be found at https://github.com/herseyc/SVT/blob/master/SVT-BackupVM.ps1
+#
+# http://www.vhersey.com/
+# 
+#######################################################################
 #Parameters
 $ovc = "IPAddressofOVC"
 $user = "vCenterUser@domain"
@@ -5,7 +16,7 @@ $pass = "vCenterUserPassword"
 $datacenter = "SVTDatacenterToStoreBackup" 
 $retention = "525600" # Retention Period in Minutes ie: 525600 = 1 year
 
-#List of VMs to Backup
+#List of VMs to Backup - update the path to the location of the list of VMs
 $vmList = Get-Content C:\temp\simplivity.csv
 
 #Backup each VM in list
@@ -14,7 +25,8 @@ foreach ($vmName in $vmList)
   # Create a Time Stamp to use in the backup name MonthDayYearHourMinute
   $now = Get-Date
   $dtstamp = "$($now.Month)$($now.Day)$($now.Year)$($now.Hour)$($now.Minute)" 
-  $backupname = "$vmName-BU-$dtstamp"
+  # Create the backup name - VMname-APIBU-DateTimeStamp
+  $backupname = "$vmName-APIBU-$dtstamp"
 
   Write-Host "Backing Up VM $vmName"
     
