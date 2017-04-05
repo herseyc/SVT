9d8d959da741c71351ac6882c9b5d4f933a3016a
Hersey <herseyc@gmail.com>
2017-04-05 08:13:42 -0400

    Create SVT-Auth.ps1

---
+++ functions/SVT-Auth.ps1
@@ -0,0 +1,15 @@
+function SVT-AuthHeaders ($SVTovc, $SVTuser, $SVTpass) {
+   # Authenticate - Get SVT Access Token
+   $uri = "https://" + $SVTovc + "/api/oauth/token"
+   $base64 = [Convert]::ToBase64String([System.Text.UTF8Encoding]::UTF8.GetBytes("simplivity:"))
+   $body = @{username="$SVTuser";password="$SVTpass";grant_type="password"}
+   $headers = @{}
+   $headers.Add("Authorization", "Basic $base64") 
+   $response = Invoke-RestMethod -Uri $uri -Headers $headers -Body $body -Method Post 
+   $atoken = $response.access_token
+   $headers = @{}
+   $headers.Add("Authorization", "Bearer $atoken")
+   return ,$headers
+}
+
+$restHeaders = SVT-Auth($ovc, $username, $SVTpass)
